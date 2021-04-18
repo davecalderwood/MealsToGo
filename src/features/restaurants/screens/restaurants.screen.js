50d904272a7c6a3554f6c45cf949386d5a32452f
Dave Calderwood <davidhcalderwood@gmail.com>
2021-04-18 17:24:40 -0400

    Loading container

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,13 +1,14 @@
 import React, { useContext } from "react";
 import { Searchbar } from "react-native-paper";
-import { FlatList } from "react-native";
+import { FlatList, View } from "react-native";
+import { ActivityIndicator, Colors } from 'react-native-paper';
 import styled from "styled-components/native";
-
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
 
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { colors } from "../../../infrastructure/theme/colors";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
@@ -19,11 +20,30 @@ const RestaurantList = styled(FlatList).attrs({
   },
 })``;
 
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
   console.log(error);
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer style={{ position: 'absolute', top: '50%', left: '50%'}}>
+          <Loading
+            size={50}
+            style={{ marginLeft: -25 }}
+            animating={true}
+            colors={Colors.blue300}
+          />
+        </LoadingContainer>
+      )}
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
