ac38b20e09c7743bc20b1f48b7d4b90cda2fe301
Dave Calderwood <davidhcalderwood@gmail.com>
2021-04-20 16:18:26 -0400

    Search bar

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,18 +1,13 @@
 import React, { useContext } from "react";
-import { Searchbar } from "react-native-paper";
 import { FlatList, View } from "react-native";
 import { ActivityIndicator, Colors } from 'react-native-paper';
 import styled from "styled-components/native";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
-
+import { Search } from '../components/search.component';
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 import { colors } from "../../../infrastructure/theme/colors";
-
-const SearchContainer = styled.View`
-  padding: ${(props) => props.theme.space[3]};
-`;
 
 const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: {
@@ -31,7 +26,6 @@ const LoadingContainer = styled.View`
 
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
-  console.log(error);
   return (
     <SafeArea>
       {isLoading && (
@@ -44,9 +38,9 @@ export const RestaurantsScreen = () => {
           />
         </LoadingContainer>
       )}
-      <SearchContainer>
-        <Searchbar />
-      </SearchContainer>
+
+      <Search />
+
       <RestaurantList
         data={restaurants}
         renderItem={({ item }) => {
